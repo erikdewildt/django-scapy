7276bf245a29d13bb91133bfbe0d5d85564f13d8
Erik de Wildt <erik.de.wildt@gmail.com>
2015-08-05 12:37:46 +0200

    Implemented messages in template Display message when no capture file is selected Register capture file being worked on in session Display capture file being worked on in 2nd level menu

--- captures/views.py
+++ captures/views.py
@@ -1,7 +1,11 @@
+from django.contrib import messages
 from django.db.models import Sum
+from django.http import Http404
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.detail import DetailView
+
 from django.core.urlresolvers import reverse_lazy
 
 from .forms import CaptureForm
@@ -59,4 +63,25 @@ class CaptureDetail(DetailView):
     Show Capture file details
     """
     model = Capture
+    allow_empty = True
 
+    def get(self, request, *args, **kwargs):
+        """
+        Redirect to list page if no object is found
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        try:
+            self.object = self.get_object()
+            # Register capture_id and capture_name in session
+            request.session['capture_id'] = self.object.id
+            request.session['capture_name'] = self.object.name
+        except Http404:
+            # redirect here
+            messages.add_message(request, messages.ERROR, 'No capture file selected. Please select a capture file first.')
+            url = reverse_lazy('captures:capture-list')
+            return redirect(url)
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
