245505b1710a2168ea05eeda9b6f4e8559376cbd
Rene Dohmen <acidjunk@gmail.com>
2015-08-08 13:19:02 +0200

    Added a temporary homepage

--- djangoscapy/urls.py
+++ djangoscapy/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from captures import views # include temp homepage
 
 urlpatterns = patterns('',
+                       url(r'^$', views.CaptureList.as_view(), name='home'), # temp home page
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^captures/', include('captures.urls', namespace='captures')),
                        )
